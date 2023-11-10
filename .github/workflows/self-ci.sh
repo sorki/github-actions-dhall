a64f159cd69086b4f648a6467261adb84b4f1927
Richard Marko <srk@48.io>
2023-11-10 11:43:10 +0100

    Script cleanup

--- .github/workflows/self-ci.sh
+++ .github/workflows/self-ci.sh
@@ -2,8 +2,9 @@
 # Script by @fisx
 
 set -eo pipefail
+
+# cd into the dir where this script is placed
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 echo "regenerating .github/workflows/dhall.yaml..."
-
-dhall-to-yaml-ng --file self-ci.dhall > .github/workflows/dhall.yaml
+dhall-to-yaml-ng --file ../../self-ci.dhall > dhall.yaml
