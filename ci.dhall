300f14dcbfdcae5acea0d2b43ea630de59c36d66
Richard Marko <srk@48.io>
2023-11-11 17:38:42 +0100

    README: Usage
    Also
    * drop ci.dhall
    * don't add ... after echo in ci.sh

--- ci.dhall
+++
@@ -1,9 +0,0 @@
-let haskellCi = ./haskell-ci.dhall
-
-in    haskellCi.generalCi
-        haskellCi.defaultCabalSteps
-        haskellCi.DhallMatrix::{
-        , ghc =
-          [ haskellCi.GHC.GHC963, haskellCi.GHC.GHC947, haskellCi.GHC.GHC928 ]
-        }
-    : haskellCi.CI.Type
--- ci.sh
+++ ci.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 # cd into the dir where this script is placed
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-echo "regenerating .github/workflows/ci.yaml..."
+echo "regenerating .github/workflows/ci.yaml"
 
 which dhall-to-yaml-ng || cabal install dhall-yaml
 dhall-to-yaml-ng --generated-comment --file ci.dhall > ci.yaml
